333981c39a3dc7c08e5e5c3243f343021e993441
Andrew Yang <zyang256@ucsc.edu>
2026-02-11 16:38:00 -0800

    Fix the video 3.2 bug

--- Asgn3/src/Cube.js
+++ Asgn3/src/Cube.js
@@ -27,29 +27,29 @@ class Cube {
         drawTriangle3DUV([0, 0, 0, 0, 1, 0, 1, 1, 0], [0, 0, 0, 1, 1, 1]);
 
         //drawTriangle([0,0,0,  1,1,0,  1,0,0]);
-        drawTriangle([0,0,0, 0,1,0, 1,1,0]);
+        //drawTriangle([0,0,0, 0,1,0, 1,1,0]);
 
         gl.uniform4f(u_FragColor, rgba[0] * .9, rgba[1] * .9, rgba[2] * .9, rgba[3]);
 
         //top of cude
-        drawTriangle3DUV([0,1,0, 0,1,1, 1,1,1]);
-        drawTriangle3DUV([0, 1, 0, 1, 1, 1, 1, 1, 0]);
+        drawTriangle3DUV([0, 1, 0, 0, 1, 1, 1, 1, 1], [1, 0, 0, 1, 1, 1]);
+        drawTriangle3DUV([0, 1, 0, 1, 1, 1, 1, 1, 0], [0, 0, 0, 1, 1, 1]);
 
         // Back face
-        drawTriangle3DUV([0, 0, 1, 1, 0, 1, 1, 1, 1]);
-        drawTriangle([0, 0, 1, 1, 1, 1, 0, 1, 1]);
+        drawTriangle3DUV([0, 0, 1, 1, 0, 1, 1, 1, 1], [1, 0, 0, 1, 1, 1]);
+        drawTriangle3DUV([0, 0, 1, 1, 1, 1, 0, 1, 1], [0, 0, 0, 1, 1, 1]);
 
         // Bottom face
-        drawTriangle3DUV([0, 0, 0, 1, 0, 0, 1, 0, 1]);
-        drawTriangle3DUV([0, 0, 0, 1, 0, 1, 0, 0, 1]);
+        drawTriangle3DUV([0, 0, 0, 1, 0, 0, 1, 0, 1], [1, 0, 0, 1, 1, 1]);
+        drawTriangle3DUV([0, 0, 0, 1, 0, 1, 0, 0, 1], [0, 0, 0, 1, 1, 1]);
 
         // Left face
-        drawTriangle3DUV([0, 0, 0, 0, 1, 0, 0, 1, 1]);
-        drawTriangle3DUV([0, 0, 0, 0, 1, 1, 0, 0, 1]);
+        drawTriangle3DUV([0, 0, 0, 0, 1, 0, 0, 1, 1], [1, 0, 0, 1, 1, 1]);
+        drawTriangle3DUV([0, 0, 0, 0, 1, 1, 0, 0, 1], [0, 0, 0, 1, 1, 1]);
 
         // Right face
-        drawTriangle3DUV([1, 0, 0, 1, 1, 0, 1, 1, 1]);
-        drawTriangle3DUV([1, 0, 0, 1, 1, 1, 1, 0, 1]);
+        drawTriangle3DUV([1, 0, 0, 1, 1, 0, 1, 1, 1], [1, 0, 0, 1, 1, 1]);
+        drawTriangle3DUV([1, 0, 0, 1, 1, 1, 1, 0, 1], [0, 0, 0, 1, 1, 1]);
 
     }
 }
